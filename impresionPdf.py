5c05a886da293ada27bc3623a86d17799c44b87e
Esteban Montoya <estebanmontya14@gmail.com>
2025-01-25 17:49:41 -0500

    Se realizan correcciones en el frontend sobre los campos en formato de moneda, el funcionamiento de las imagenes y el funcionamiento de la tabla para totalizar, funcionar con formatos de moneda y con formatos de miles, tambien se realizan pruebas de impresion desde python con la impresora de tickets

---
+++ impresionPdf.py
@@ -0,0 +1,44 @@
+import win32print
+import win32api
+import os
+
+# Requiere que este inslado acrobat reader
+
+# Nombre de la impresora
+printer_name = "EPSON TM-T20II Receipt"  # Asegúrate de colocar el nombre correcto de la impresora
+
+# Ruta del archivo PDF
+pdf_path = "./Factura_Termal.pdf"
+
+# Abre la impresora
+handle = win32print.OpenPrinter(printer_name)
+
+# Crea un trabajo de impresión
+job = win32print.StartDocPrinter(handle, 1, ("Impresión de Factura", None, "RAW"))
+win32print.StartPagePrinter(handle)
+
+# Usamos el comando "start" para enviar el archivo PDF a la impresora
+# Esto usa el programa predeterminado de Windows para manejar la impresión de PDFs (como Adobe Reader o Edge)
+command = f'cmd /c start /min Acrobat.exe /t "{pdf_path}" "{printer_name}"'
+os.system(command)  # Envia el comando al sistema
+
+# Finaliza la página e imprime
+win32print.EndPagePrinter(handle)
+win32print.EndDocPrinter(handle)
+win32print.ClosePrinter(handle)
+
+
+
+
+
+# import os
+
+# # Ruta al archivo PDF
+# pdf_path = "C:/Users/User/Desktop/desarrollo/Proyectos de software/optica/Factura_Termal.pdf"
+
+# # Nombre de la impresora
+# printer_name = "EPSON TM-T20II Receipt5"
+
+# # Usamos el comando "start" para abrir el PDF en el visor predeterminado y enviar a la impresora
+# command = f'cmd /c start /min msedge.exe /t "{pdf_path}" "{printer_name}"'
+# os.system(command)
--- optica/contabilidad/admin.py
+++ optica/contabilidad/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.contrib import admin
-from .models import MediosDePago, EstadoVenta, Ventas, Abonos, Articulos 
+from .models import MediosDePago, EstadoVenta, Ventas, Abonos, Articulos, FotosArticulos
 from django.forms import NumberInput
+from django.utils.html import mark_safe
+
+from rich.console import Console
+console = Console()
+
+# from PIL import Image
 # Register your models here.
 
 # Register your models here.
@@ -56,18 +62,61 @@ class EstadoVentaAdmin(admin.ModelAdmin):
     )
 
 
+
+
+admin.site.register(FotosArticulos)
+
+class FotosInline(admin.StackedInline):
+    model = FotosArticulos
+    extra = 0
+    readonly_fields = ['cover_preview']
+
+    def cover_preview(self, obj):
+        # console.log("activadp")
+        # console.log(obj)
+        # console.log(obj.foto)
+        if obj:
+            return mark_safe(f'<img src="http://localhost:8000/media/{obj.foto}" style="width: 200px; height: auto;" />')
+        return "No image available"
+
+    cover_preview.short_description = "Cover Preview"
+
+
+
 @admin.register(Articulos)
 class ArticulosAdmin(admin.ModelAdmin):
+    inlines = [FotosInline]
+
     list_display = (
         'id',
         'nombre',
         'descripcion',
         'precio',
-        'foto',
+        # 'foto',
+        'cover_preview',
     )
 
     class Media:
         js = ('js/utilsAdmin.js',)
+
+    
+    def cover_preview(self, obj):
+        # Verifica si el artículo tiene imágenes asociadas
+        console.log(obj.articuloFoto.all())
+        try:
+            fotos = obj.articuloFoto.all() #first()  # Obtén la primera foto relacionada
+            # console.log(fotos.foto)
+            if len(fotos) > 0:  # Verifica si existe una foto
+                # return mark_safe(f'<img src="http://localhost:8000/media/{fotos.foto}" style="width: 100px; height: auto;" />')
+                imagenes_html = ''.join([
+                    f'<img src="http://localhost:8000/media/{foto.foto}" style="width: 100px; height: auto; margin-right: 5px;" />'
+                    for foto in fotos
+                ])
+                console.log(imagenes_html)
+                return mark_safe(imagenes_html)
+            return "No image available"
+        except:
+            pass
 
     
 # @admin.register(MediosDePago)
